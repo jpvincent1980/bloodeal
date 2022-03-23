f1a9f796971f34d33e1f2d8aac5b6cead17ed5da
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2022-03-23 12:37:37 +0100

    Add CustomUser model to admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import CustomUser
+
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    """
+    A class inheriting from ModelAdmin to manage Users instances in the Admin
+    interface.
+    """
+    list_display = ("id", "email", "first_name", "last_name", "is_staff")
+    list_editable = ("email",)
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,48 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    """
+    Define a model manager for CustomUser model with no username field.
+    """
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a CustomUser with the given email and password."""
+        if not email:
+            raise ValueError('Vous devez saisir une adresse email valide.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create and save a regular CustomUser with the given email and
+        password.
+        """
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
 class CustomUser(AbstractUser):
     """
     A model that represents a user
@@ -12,3 +52,6 @@ class CustomUser(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
