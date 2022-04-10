9821d48e1ff1d0588a40f7ee082430e07ab8b7d3
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2022-04-10 19:24:39 +0200

    Removing fake test from movies app + adding real tests in accounts app + adding --disable-warnings from pytest command in CircelCI config.yml file

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from accounts.models import CustomUser
+
 
 # Create your tests here.
+class AccountsTest(TestCase):
+    def setUp(self) -> None:
+        self.user = CustomUser.objects.create(email="test@gmail.com")
+        self.user.set_password("azerty01")
+        self.user.save()
+
+    def test_index_anonymous_user(self):
+        url = reverse("accounts:index")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "deals/deals_list.html")
+        self.assertNotIn("PROFIL", response.content.decode())
+
+    def test_index_authenticated_user(self):
+        self.client.login(email="test@gmail.com",
+                          password="azerty01")
+        url = reverse("accounts:index")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "deals/deals_list.html")
+        self.assertIn("PROFIL", response.content.decode())
--- movies/tests.py
+++ movies/tests.py
@@ -2,7 +2,4 @@ from django.test import TestCase
 
 
 # Create your tests here.
-class MovieTest(TestCase):
 
-    def test(self):
-        assert(200 == 200)
